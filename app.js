0b3a3ea961e24239dc1a2e1b41dfce4f297687c2
Doni Setiawan <donisetiawan.050@gmail.com>
2020-02-27 19:10:09 +0700

    Installing the package

---
+++ app.js
@@ -0,0 +1,8 @@
+const io = require('socket.io').listen(4000);
+
+io.sockets.on('connection', (socket) => {
+  socket.emit('ping');
+  socket.on('pong', (data) => {
+    console.log('pong');
+  });
+});
